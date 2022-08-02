66b8ead7f5ae8a1306dbaa3cf4c8ad247ef326b4
hariturbo44 <hariturbo1234@gmail.com>
2022-08-03 00:48:06 +0800

    Create mainAdmin.java

---
+++ C206_CaseStudy/src/mainAdmin.java
@@ -0,0 +1,27 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * harit, 3 Aug 2022 12:47:38 am
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * harit, 3 Aug 2022 12:47:38 am
+ */
+
+public class mainAdmin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
