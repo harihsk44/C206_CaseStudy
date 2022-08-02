cd85aa4b323aee4d438f8be434cbd37ace222889
hariturbo44 <hariturbo1234@gmail.com>
2022-08-03 00:55:02 +0800

    Update mainAdmin.java

--- C206_CaseStudy/src/mainAdmin.java
+++ C206_CaseStudy/src/mainAdmin.java
@@ -1,27 +1,43 @@
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * harit, 3 Aug 2022 12:47:38 am
- */
 
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * harit, 3 Aug 2022 12:47:38 am
- */
 
 public class mainAdmin {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	private void displaymenu() {
+		System.out.println("Edit Stall");
+		System.out.println("Edit Food Item");
+		System.out.println("Edit Food Item");
+		System.out.println("View Promotion Offer");
+		System.out.println("View Ingredient Request Order");
+		System.out.println("Purchase Ingredient Online");
+		System.out.println("Generate Sales Report");
+	}
+	
+	private void doEditStall() {
+		
+	}
+	
+	private void doEditFoodItem() {
+		
+	}
+	
+	private void doViewPromotionOffer() {
+		//view promotion offers
+	}
+	
+	private void doViewIngredientRequestOrder() {
+		
+	}
+	
+	private void doPurchaseIngredientOnline() {
+		
+	}
+	
+	private void doGenerateSalesReport() {
+		
+	}
 }
