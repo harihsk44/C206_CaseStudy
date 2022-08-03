37328de6ef69844227f76cbd41dca73c7bcec0a6
hariturbo44 <hariturbo1234@gmail.com>
2022-08-03 12:00:31 +0800

    Update mainOperator.java

--- C206_CaseStudy/src/mainOperator.java
+++ C206_CaseStudy/src/mainOperator.java
@@ -1,6 +1,8 @@
-
+import java.util.ArrayList;
 
 public class mainOperator {
+	
+	private ArrayList<Food> foodlist = new ArrayList<Food>();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
