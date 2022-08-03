9589af0f91d96a1696cb754f3f924ea16992a834
hariturbo44 <hariturbo1234@gmail.com>
2022-08-03 11:28:00 +0800

    adding helper and methods in mainOP

--- C206_CaseStudy/src/mainOperator.java
+++ C206_CaseStudy/src/mainOperator.java
@@ -2,12 +2,38 @@
 
 public class mainOperator {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		mainOperator MO = new mainOperator();
+		MO.menu();
+		int choice = Helper.readInt("Enter choice > ");
+		
 	}
+	
+	private void menu() {
+		System.out.println("1. View Food Items");
+		System.out.println("2. Manage Promotion Offer");
+		System.out.println("3. Manage Orders");
+		System.out.println("4. Manage Request Orders of ingredients");
+		System.out.println("5. Manage promotion Offers ");
+	}
+	
+	private void doviewfood() {
+		
+	}
+	
+	private void promotionsubmenu() {
+		
+	}
+	
+	private void ordersubmenu() {
+		
+	}
+	
+	private void ingredientsubmenu() {
+		
+	}
+	
 
 }
